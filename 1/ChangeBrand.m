bc064c990f43d8a93d279d055c4027424349b9ae
mbertolino <mattias.bertolino@gmail.com>
2017-03-24 14:14:17 +0100

    Updated 1.1

--- 1/ChangeBrand.m
+++ 1/ChangeBrand.m
@@ -1,29 +1,21 @@
 function u = ChangeBrand(p, N, u1, T)
 %CHANGEBRAND Summary of this function goes here
-%   Detailed explanation goes here
+%   u - Number of iPhone users
 
 u = zeros(1, T);
 u(1) = u1;
 
-for t = 1:T-1
-    u(t+1) = u(t);
-    
-    % Changing to iPhone
-    for i = 1:u(t)
-        % Probability of changing brand
-        if rand < p
-            u(t+1) = u(t+1) - 1;
-        else
-            % Get influenced by others
-        end    
+for t = 1:T-1    
+    if rand < u(t)/N
+        %disp('iPhone broke');
+        u(t+1) = u(t) - 1;
+    else
+        %disp('Other brand broke');
+        u(t+1) = u(t);
     end
     
-    % Changing to other brand
-    for i = u(t)+1:N
-        if rand < p
-            u(t+1) = u(t+1) + 1;
-        else
-            % Get influenced by others
-        end
+    % Buy new phone
+    if rand < p
+        u(t+1) = u(t+1) + 1;
     end
 end
--- 1/fashion.m
+++ 1/fashion.m
@@ -4,10 +4,23 @@ clear all; close all; clc;
 T = 10000;
 N = 100; % Total students 15 or 100
 p = 0.7; % 0.5 or 0.7
-u1 = 50;
+u1 = rand*N;
 
 % Changing brand
 u = ChangeBrand(p, N, u1, T);
 
+% % 1.2 Implementing master equation
+% u_m = zeros(1, N);
+% i = 1;
+% for t = 1:T-1
+%     u_m(
+%     for i = 1:(N)
+%         u_m(t, i) = nchoosek4(N, i)*p^i*(1-p)^(N-i);
+%     end
+% end
+
+% 1.3 
+
 % Plot equilibrium distribution
+figure()
 plot(1:T, u);
