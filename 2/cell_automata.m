487c0e31f2b3cb8e92f7ed008f5b175b63459903
mbertolino <mattias.bertolino@gmail.com>
2017-04-02 15:31:09 +0200

    Added comments and fixed plots to 2.1

--- 2/cell_automata.m
+++ 2/cell_automata.m
@@ -1,30 +1,41 @@
 clear all; close all; % clc;
 
 T = 70;
-rule = dec2bin(110, 8);
-x0 = dec2bin(1131^8, 160);
+% Classes: 136 37 18 110
+%init = 1024^8;
+init = round(1024^8*rand);
+rules = [136 37 18 110];
 
-% Initialize matrix
-a = zeros(T, length(x0));
-for i = 1:length(x0)
-    a(1, i) = str2double(x0(i));
-end
-
-% Update
-for t = 2:T
-    x1 = ca_func([x0(end) x0(1) x0(2)], rule);
-    for i = 2:length(x0)-1
-        x1 = [x1 ca_func([x0(i-1) x0(i) x0(i+1)], rule)];
-    end
-    x1 = [x1 ca_func([x0(end-1) x0(end) x0(1)], rule)];
-    
-    % Save in matrix
-    for i = 1:length(x0)
-        a(t, i) = str2double(x1(i));
-    end
-    x0 = x1;
-end
-
-% Plot development
+% Repeat for each class
+figure()
 colormap(gray)
-imagesc(imcomplement(a))
+for c = 1:4
+    rule = dec2bin(rules(c), 8);
+    x0 = dec2bin(init, 161);
+    
+    % Initialize matrix
+    a = zeros(T, length(x0));
+    for i = 1:length(x0)
+        a(1, i) = str2double(x0(i));
+    end
+    
+    % Update
+    for t = 2:T
+        x1 = ca_func([x0(end) x0(1) x0(2)], rule);
+        for i = 2:length(x0)-1
+            x1 = [x1 ca_func([x0(i-1) x0(i) x0(i+1)], rule)];
+        end
+        x1 = [x1 ca_func([x0(end-1) x0(end) x0(1)], rule)];
+        
+        % Save in matrix
+        for i = 1:length(x0)
+            a(t, i) = str2double(x1(i));
+        end
+        x0 = x1;
+    end
+    
+    % Plot development
+    subplot(2, 2, c)
+    imagesc(imcomplement(a))
+    title(['Rule ' num2str(rules(c))])
+end
