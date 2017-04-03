8dd5fce6b307401a83733e8a0b35c30e11e60841
mbertolino <mattias.bertolino@gmail.com>
2017-04-03 14:41:11 +0200

    2 done

--- 2/n_painter.m
+++ 2/n_painter.m
@@ -1,28 +1,29 @@
-clear all; %close all; clc;
+clear all; % close all; clc;
+ip = 4;
+n = 4;
 
-T = 10000;
-N = 100;
+T = 1000;
+N = 200;
 world = zeros(N, N);
 
 % Colors and rules
-color = [0 12 24 36 48 60 72 84 96];
-turn = [1 1 1 1 -1 1 1 1 1]; % 1=left, -1=right
+color = [0 12 24];
+turn = [1 1 -1]; % 1=left, -1=right
 
 % Initial coordinates of painter
 x_old = N/2;
 x_new = N/2;
-y_old = N/2;
-y_new = N/2 - 1;
+y_old = N/2 + 1;
+y_new = N/2;
 world(y_new, x_new) = color(1);
 
 % Step in time and paint
-figure()
-colormap('jet')
 for t = 1:T
     % Update coordinates
     x = x_new;
     y = y_new;
     
+    % Paint :)
     idx = world(y, x) == color;
     world(y, x) = color(circshift(idx, 1));
     
@@ -31,11 +32,11 @@ for t = 1:T
     y_new = y + turn(idx)*(x - x_old);
     x_old = x;
     y_old = y;
-    
-    % Paint :)
-    %image(world)
-    %pause(0.002);
 end
 
 % Final world
-image(world);
+%figure()
+colormap('jet')
+subplot(2, 2, ip)
+imagesc(imcomplement(world));
+title([num2str(n)])
--- 2/painter.m
+++ 2/painter.m
@@ -1,48 +1,41 @@
-clear all; close all; clc;
+clear all; % close all; clc;
 
-T = 18000;
-N = 300;
+T = 11000;
+N = 100;
 world = zeros(N, N);
 
+% Colors and rules
+color = [0 1];
+turn = [1 -1]; % 1=left, -1=right
+
 % Initial coordinates of painter
-x_old = 100;
-x_new = 100;
-y_old = 101;
-y_new = 100;
-world(y_new, x_new) = 1;
+x_old = N/2;
+x_new = N/2;
+y_old = N/2 + 1;
+y_new = N/2;
+world(y_new, x_new) = color(1);
 
 % Step in time and paint
-figure()
-colormap('gray')
 for t = 1:T
     % Update coordinates
     x = x_new;
     y = y_new;
     
-    % If white
-    if (world(y, x) == 0)
-       world(y, x) = 1;
-       
-       % Turn right
-       x_new = x + (y_old - y); % x = 10 + (10 - 10) = 10
-       y_new = y + (x - x_old); % y = 10 + (10 - 9) = 11
-    else
-        world(y, x) = 0;
-
-        % Turn left
-        x_new = x + (y - y_old);
-        y_new = y + (x_old - x);
-    end
+    % Paint :)
+    idx = world(y, x) == color;
+    world(y, x) = color(circshift(idx, 1));
     
-    % Update old coordinates
+   % Update old coordinates
+    x_new = x + turn(idx)*(y_old - y);
+    y_new = y + turn(idx)*(x - x_old);
     x_old = x;
     y_old = y;
-    
-    % Paint :)
-    %imagesc(imcomplement(world))
-    %pause(0.002);
 end
 
 % Final world
+figure()
+colormap('gray')
 imagesc(imcomplement(world));
 title(['Time: ' num2str(T)])
+xlabel('x-coordinates')
+ylabel('y-coordinates')
