d3660efe4d34da0e8e04d6fafabd723385d329e8
mbertolino <mattias.bertolino@gmail.com>
2017-04-11 23:10:58 +0200

    Started 3

--- 3/popdyn.m
+++ 3/popdyn.m
@@ -1,36 +1,59 @@
+clear all; close all;
 
 % Param
-b = 10; % Newborn at each site populated by 2
+N_sims = 20;
+b = [5 10 20 30 40]; % Newborn at each site populated by 2
 n = 1000; % Number of sites
-T = 20;
-A = zeros(T, 1); % Population
-A(1) = n;
+T = 100;
 mf = zeros(T, 1);
 mf(1) = n;
+A = zeros(T, N_sims); % Population
 
-for t = 2:T
-    % All individuals choose which site to visit
-    site = round((n-1)*rand(1, A(t-1))) + 1;
+% Figure param
+figure()
+hold on;
+color = ['b', 'r', 'k', 'g', 'c'];
+
+% Setup waitbar
+h = waitbar(0, 'Please wait...');
+
+for k = 1:length(b)
+    A(1, :) = n;
     
-    % Reproduce or disappear
-    for i = 1:A(t-1)
-        if sum(site == i) == 2
-            A(t) = A(t) + b;
+    for j = 1:N_sims
+        for t = 2:T
+            % All individuals choose which site to visit
+            %site = round((n-1)*rand(1, A(t-1))) + 1;
+            site = randi([1, n], 1, A(t-1, j));
+            
+            % Reproduce or disappear
+            for i = 1:A(t-1, j)
+                if sum(site == i) == 2
+                    A(t, j) = A(t, j) + b(k);
+                end
+            end
+            
+            % Mean field equation
+            %     for i = 1:A(t)/2
+            %         mf(t) = mf(t) + (A(t)/n.^2).^i/(factorial(i-1));
+            %     end
+            %     mf(t) = b(k)*exp(-A(t)/n.^2)*mf(t);
+            %     mf(t) = b(k)*n*nchoosek(mf(t-1), 2)*(1./n).^2*((n-1)./n).^(mf(t-1)-2)
         end
+            % Update waitbar
+        waitbar(j/N_sims)
     end
     
-    % Mean field equation
-%     for i = 1:A(t)/2
-%         mf(t) = mf(t) + (A(t)/n.^2).^i/(factorial(i-1));
-%     end
-%     mf(t) = b*exp(-A(t)/n.^2)*mf(t);
-    mf(t) = b*n*nchoosek(mf(t-1), 2)*(1./n).^2*((n-1)./n).^(mf(t-1)-2)
+    % Plot population
+    plot(1:T, A, color(k))
     
 end
+close(h)
 
-% Plot population
-figure()
-plot(1:T, A)
-hold on;
-figure()
-plot(1:T, mf)
+xlabel('Time')
+ylabel('Population')
+legend('b = 5', 'b = 10', 'b = 20', 'b = 30', 'b = 40')
+
+% hold on;
+% figure()
+% plot(1:T, mf)
