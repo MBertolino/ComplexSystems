8e06ebbb346635158136e58a0eeca8ed75b1c52e
mbertolino <mattias.bertolino@gmail.com>
2017-04-14 12:06:40 +0200

    Solved 3.4 local population dynamics, almost done

---
+++ 3/pop_local_func.m
@@ -0,0 +1,30 @@
+function A = pop_local_func(n, b, T, A1, d)
+
+A = zeros(T, 1); % Population
+A(1) = A1;
+
+% All individuals choose which site to visit
+site = randi([1, n], 1, A(1));
+
+% Reproduce or disappear
+idx = histc(site, 1:n);
+idx = (idx == 2);
+A(2) = sum(idx)*b;
+
+for t = 3:T
+    site = zeros(1, n);
+    for i = find(idx == 1)
+        move = i-d:i+d;
+        move(move > n) = move(move > n) - n;
+        move(move < 1) = n + move(move < 1);
+        
+        % All individuals choose which site to visit locally [i-d, i+d]
+        pos = randi(length(move), 1, b);
+        site_temp = move(pos);
+        site(unique(move)) = site(unique(move)) + histc(site_temp, unique(move));
+    end
+    
+    % Reproduce or disappear
+    idx = (site == 2);
+    A(t) = A(t) + sum(idx == 1)*b;
+end
--- 3/popdyn.m
+++ 3/popdyn.m
@@ -1,22 +1,23 @@
-% clear all; close all;
+clear all; close all;
 
 % Param
-b = 1:50; %[5 10 20 30 40]; % Newborn at each site populated by 2
-n = 1000; % Number of sites
-T = 100;
-N_sims = 100;
+b = 20; %[5 10 20 30 40]; % Newborn at each site populated by 2
+n = 500; % Number of sites
+d = 100; % How far they move [1, n/2]
+T = 500;
+N_sims = 1;
 A_range = 0:15000;
 
 A = zeros(T, 1); % Population
 A_eq = zeros(N_sims, length(b));
 A_dyn = zeros(A_range(end), length(b));
+A(1) = n;
 mf = zeros(T, 1);
 mf(1) = n;
 
 % Figure param
-% figure()
-% hold on;
-% color = ['b', 'r', 'k', 'g', 'm'];
+figure()
+hold on;
 
 % Setup waitbar
 h = waitbar(0, 'Please wait...');
@@ -25,41 +26,42 @@ h = waitbar(0, 'Please wait...');
 for j = 1:length(b)
     for i = 1:N_sims
         % Calculate A
-        A = pop_func(n, b(j), T, A1);
+        A = pop_local_func(n, b(j), T, A(1), d);
+        %         A = pop_func(n, b(j), T, A(1));
         A_eq(i, j) = A(end);
     end
     
-    % Bifurcation diagram  
+    % Bifurcation diagram
     for k = 0:(max(A_eq(:, j))-1)
         A_dyn(k+1, j) = sum(A_eq(:, j) == k)/N_sims;
     end
     
     % Plot population
-    % plot(1:T, A, color(k))
+    plot(1:T, A)
     
     % Update waitbar
     waitbar(j/N_sims)
 end
 close(h)
 
-% xlabel('Time')
-% ylabel('Population size')
-% legend('b = 5', 'b = 10', 'b = 20', 'b = 30', 'b = 40')
+xlabel('Time')
+ylabel('Population size')
+legend('b = 5', 'b = 10', 'b = 20', 'b = 30', 'b = 40')
 
 % 3.1 Bifurcation diagram
-figure()
-colormap hot
-imagesc(b, A_range, A_dyn, [0 0.3])
-xlabel('Parameter b')
-ylabel('Relative frequency')
-colorbar
+% figure()
+% colormap hot
+% imagesc(b, A_range, A_dyn, [0 0.3])
+% xlabel('Parameter b')
+% ylabel('Relative frequency')
+% colorbar
 
 
 % 3.2 Mean-field equation
-A_mf(1) = n;
-for i = 2:length(b)
-    A_mf(i) = 0.5*b(i)*exp(A_mf(t-1)/n)*A_mf(t-1).^2/n;
-end
-hold on;
-figure()
-plot(1:T, mf)
+% A_mf(1) = n;
+% for i = 2:length(b)
+%     A_mf(i) = 0.5*b(i)*exp(-A_mf(t-1)/n)*(A_mf(t-1)/n).^2;
+% end
+% hold on;
+% figure()
+% plot(1:T, mf)
