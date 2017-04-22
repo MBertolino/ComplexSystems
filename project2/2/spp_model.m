58a8fd9b6e511b259c4e0be39507b61ff4646294
mbertolino <mattias.bertolino@gmail.com>
2017-04-22 16:46:18 +0200

    Added Alignment measure

--- project2/2/spp_model.m
+++ project2/2/spp_model.m
@@ -2,12 +2,12 @@ clear all;
 
 % Param
 L = 20;
-N = 5; % Individuals
+N = 50; % Individuals
 R = 2; % Neighbor distance
-p = 0.3;
-q = p + 0.3;
+p = 0.1;
+q = p + 0:0.1:0.9;
 delta = 0.1;
-eta = 0.5;
+eta = 0.1;
 T = 1000;
 
 % Initialize position and velocities
@@ -17,67 +17,76 @@ status(:, 3) = status(:, 3)*2*pi;
 status(:, 4) = 0;
 theta_new = zeros(N, 1); % theta_i(t+1)
 
-% Simulate
-figure()
-plot(status(:, 1), status(:, 2), '*')
-xlim([0 20])
-ylim([0 20])
-pause(0.1)
-for t = 2:T
-    for n = 1:N
-        % Find neighbors
-        dist = sqrt((status(n, 1) - status(:, 1)).^2 + (status(n, 2) - status(:, 2)).^2);
-        neigh = status((dist > 0) & (dist < R), :);
-        
-        % Follow neighbors with probability p or q
-        chance = rand;
-        noise = rand*eta - eta/2;
-        if (~isempty(neigh))
-            follow_me = randi([1 size(neigh, 1)], 1, 1);
-        else
-            chance = 2;
-            theta_new(n) = status(n, 3) - noise;
-        end
-        
-        % Face neighbor
-        if (chance < p)
-            neigh_x = neigh(follow_me, 1);
-            neigh_y = neigh(follow_me, 2);
-            dir_new = pi + tan((neigh_y - status(n, 2))/(neigh_x - status(n, 1)));
-            theta_new(n) = dir_new - floor(dir_new);
-        end
-        
-        % Same direction
-        if (chance < q && chance > p)
-            dir_new = pi + neigh(follow_me, 3);
-            theta_new(n) = dir_new - floor(dir_new);
-        end
-        
-        % Add noise
-        theta_new(n) = theta_new(n) + noise;
-    end
-    % Update directions
-    status(:, 3) = theta_new;
-    
-    % Update positions
-    dir(:, 1) = cos(status(:, 3));
-    dir(:, 2) = sin(status(:, 3));
-    status(:, 4) = sqrt(dir(:, 1).^2 + dir(:, 2).^2);
-    status(:, [1 2]) = status(:, [1 2]) + delta*dir(:, [1 2])./status(:, 4);
-    
-    % Periodic boundaries
-    status(status(:, 1) > L, 1) = status(status(:, 1) > L, 1) - ...
-        floor(status(status(:, 1) > L, 1));
-    status(status(:, 2) > L, 2) = status(status(:, 2) > L, 2) - ...
-        floor(status(status(:, 2) > L, 2));
-    status(status(:, 1) < 0, 1) = L;
-    status(status(:, 2) < 0 , 2) = L;
-    
-    % Plot all individuals
-    plot(status(:, 1), status(:, 2), '*')
-    xlim([0 20])
-    ylim([0 20])
-    
-    pause(0.01)
-end
+psi = zeros(T, length(q));
 
+% Simulate
+% figure()
+% plot(status(:, 1), status(:, 2), '*')
+% xlim([0 20])
+% ylim([0 20])
+% pause(0.1)
+for iq = 1:length(q)
+    psi(1, iq) = (sum(cos(status(:, 3))).^2 + sum(sin(status(:, 3))).^2)/N;
+    for t = 2:T
+        for n = 1:N
+            % Find neighbors
+            dist = sqrt((status(n, 1) - status(:, 1)).^2 + (status(n, 2) - status(:, 2)).^2);
+            neigh = status((dist > 0) & (dist < R), :);
+            
+            % Follow neighbors with probability p or q
+            chance = rand;
+            noise = rand*eta - eta/2;
+            if (~isempty(neigh))
+                follow_me = randi([1 size(neigh, 1)], 1, 1);
+            else
+                chance = 2;
+                theta_new(n) = status(n, 3) - noise;
+            end
+            
+            % Face neighbor
+            if (chance < p)
+                neigh_x = neigh(follow_me, 1);
+                neigh_y = neigh(follow_me, 2);
+                dir_new = pi + atan((neigh_y - status(n, 2))/(neigh_x - status(n, 1)));
+                theta_new(n) = wrapTo2Pi(dir_new);
+            end
+            
+            % Same direction
+            if (chance < q(iq) && chance > p)
+                %             dir_new = pi + neigh(follow_me, 3);
+                %             theta_new(n) = dir_new - floor(dir_new);
+                theta_new(n) = status(follow_me, 3);
+            end
+            % Add noise
+            theta_new(n) = theta_new(n) + noise;
+        end
+        % Update directions
+        status(:, 3) = theta_new;
+        
+        % Update positions
+        dir(:, 1) = cos(status(:, 3));
+        dir(:, 2) = sin(status(:, 3));
+        status(:, 4) = sqrt(dir(:, 1).^2 + dir(:, 2).^2);
+        status(:, [1 2]) = status(:, [1 2]) + delta*dir(:, [1 2])./status(:, 4);
+        
+        % Periodic boundaries
+        status(status(:, 1) > L, 1) = status(status(:, 1) > L, 1) - ...
+            L;
+        status(status(:, 2) > L, 2) = status(status(:, 2) > L, 2) - ...
+            L;
+        status(status(:, 1) < 0, 1) = L + status(status(:, 1) < 0, 1);
+        status(status(:, 2) < 0, 2) = L + status(status(:, 2) < 0, 2);
+        
+        % 2.2 Alignment measure
+        psi(t, iq) = (sum(cos(status(:, 3))).^2 + sum(sin(status(:, 3))).^2)/N;
+        
+        % Plot all individuals
+        %     plot(status(:, 1), status(:, 2), '*')
+        %     xlim([0 20])
+        %     ylim([0 20])
+        
+%         pause(0.001)
+    end
+end
+figure()
+plot(q, psi)
