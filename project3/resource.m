1718f3e92dd9c4ffbd13147ffaf669a7d397d1fc
mbertolino <mattias.bertolino@gmail.com>
2017-05-18 13:32:37 +0200

    Start

--- project3/resource.m
+++ project3/resource.m
@@ -1,3 +1,32 @@
-Hej Lina, Jonas och Adam!
+clear all; close all;
 
-HEJ MATTIAS!!!!!!!!!!!!!!!! :):):):):):):):):):)
+% Params
+n = 3;
+a = 5;
+u = 1;
+mu = 500;
+p = 0.001;
+
+% Initialize data
+S = ones(2*n, 1); % Species
+R = zeros(n, 1);
+
+% Constants
+T = 1000;
+
+% Step in time
+for t = 1:T
+    % Generate a time interval
+    dt = exprnd(1/sum(S), sum(S), 1);
+    
+    % Inflow of metabolites
+    R(u) = R(u) + mu*dt;
+    
+    % Select one individual
+    s = randsample(1:length(S), 1, 1, S);
+    
+    % Reproduce
+    
+    q = 
+    
+end
