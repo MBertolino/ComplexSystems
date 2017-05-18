a89c2c2d79af15ba843de6f083940ba0da4d23f3
JonasMel <jome@protonmail.com>
2017-05-18 18:03:51 +0200

    Almost working maybe possibly, mass death too soon

--- project3/resource.m
+++ project3/resource.m
@@ -5,31 +5,34 @@ n = 3;
 a = 5;
 u = 1;
 mu = 500;
-p = 0.001;
+p = 0.0002;
 
 % Constants
-T = 100;
+T = 1000;
 
 % Initialize data
-S = zeros(2*n, T); % Species at time t
-S(:, 1) =  1;
+S = zeros(6, T); % Species at time t
+S(1:end, 1) =  1;
 R = zeros(n, 1); % Metabolites
 N = zeros(T, 1); % Total number of species at time t
-N(1) = sum(S(:, 1));
+
 % Step in time
-for t = 2:T
-    
-    
+for t = 1:T
+    N(t) = sum(S(:, t));
+    if (N(t) == 0)
+        break;
+    end
     % Generate a time interval
-    dt = exprnd(1/N(t-1), N(t-1), 1);
+    dt = exprnd(1/N(t), N(t), 1);
     
     % Inflow of metabolites
     R(u) = R(u) + mu*dt(u);
     
     for k = 1:N(t)
-        % Select one individual
-        s = randsample(1:length(S(:, t-1)), 1, 1, S(:, t-1));
         
+        % Select one individual
+        s = randsample(1:length(S(:, t)), 1, 1, S(:, t));
+%         disp(s);
         % Calculating chosen organisms speciess = 5;
         c = 0;
         for ii = 1:n
@@ -44,7 +47,9 @@ for t = 2:T
                 end
             end
         end
-        
+%         disp(i);
+%         disp(j);
+%         disp(' ');
         % Reproduce
         if (i == j)
             q = (R(i)./(a + R(i))).*((R(i) - 1)./(a + R(i) - 1));
@@ -53,24 +58,46 @@ for t = 2:T
         end
         
         % If reproduction else death
+        m = 0;
+        while ( m ~= s )
+            m = randi(length(S(:, 1)));
+        end
+        
         if (rand < q)
-            if ( (i + j) < (n + 1) )
-                S(s) = S(s) + 1;
-                R(i) = R(i + j) + 1;
-            elseif ( (i + j) == (n + 1) )
-                S(s) = S(s) + 1;
+            
+            if (rand >= p)
+                m = s;
+            end
+            if ((i+j) < (n+1))
+                S(m, t+1) = S(m, t) + 1;
+                R(i) = R(i+j) + 1;
+            elseif ((i+j) == (n+1))
+                S(m, t+1) = S(m, t) + 1;
                 R(1) = R(1) + 1;
             else
-                S(s) = S(s) + 1;
+                S(m, t+1) = S(m, t) + 1;
                 R(1) = R(1) + 1;
-                R(mod((i + j), (n + 1))) = R(mod((i + j), (n + 1))) + 1;
+                R(mod((i+j), (n+1))) = R(mod((i+j), (n+1))) + 1;
             end
             % Metabolites consumed during reproduction
             R(i) = R(i) - 1;
             R(j) = R(j) - 1;
         else
-            S(s) = S(s) - 1;
+            S(s, t+1) = S(s, t) - 1;
         end
+        
     end
-    N(t) = sum(S(:, t));
+    % Mutate species
+    %     for k = 1:length(S(:,t))
+    %        for m = 1:sum(S(k,t))
+    %            if rand < p
+    %                S(k, t+1) = S(k, t+1) -1;
+    %
+    %            end
+    %        end
+    %     end
+    
+    
 end
+N(t) = sum(S(:, t));
+plot(1:t, N(1:t));
