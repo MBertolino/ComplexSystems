c315ae928b399aab167a46a4ce15d023a0c442f6
JonasMel <jome@protonmail.com>
2017-05-19 16:24:56 +0200

    Some probable results

--- project3/resource.m
+++ project3/resource.m
@@ -6,50 +6,49 @@ a = 5;
 u = 1;
 mu = 500;
 p = 0.001;
+N_spec = n*(n+1)*0.5;
 
 % Constants
-T = 1000;
+t = 1;
+T = 500;
 
 % Initialize data
-S = zeros(n*(n+1)*0.5, T); % Species at time t
-S(1:end, 1) =  1;
+S = zeros(N_spec, T); % Species at time t
+S(:, 1) =  1;
+S_tmp = S(:, 1);
 R = zeros(n, 1); % Metabolites
 N = zeros(T, 1); % Total number of species at time t
+N(t) = sum(S(:, t));
 
 h = waitbar(0/T, 'Progress');
 % Step in time
-for t = 1:T
+while t <= T
     waitbar(t/T, h, ['Progress: ' num2str(round((t/T)*100, 1)) ' % ']);
-    % Update whole population
-    N(t) = sum(S(:, t));
-    if (N(t) == 0)
-        break;
-    end
     
     % Generate a time interval
-    dt = exprnd(1/N(t));
+    dt = exprnd(1/sum(S_tmp));
     
     % Inflow of metabolites
     R(u) = R(u) + mu*dt;
     
     % Select one individual
-    s = randsample(1:length(S(:, t)), 1, 1, S(:, t));
-    
+    s = randsample(1:N_spec, 1, 1, S_tmp);
+ 
     % Calculating chosen organisms species (the ugly way);
-    c = 0;
-    for ii = 1:n
-        for jj = ii:n
-            if (c < s)
-                c = c + 1;
-                i = ii;
-                j = jj;
-            else
-                jj = n;
-                ii = n;
-            end
+    tmp = 0;
+    for k = 0:n - 1
+        tmp = tmp + n - k;
+        if s < tmp
+            i = k + 1;
+            j = s - tmp + n;
+            break;
+        elseif s == tmp
+            i = k + 1;
+            j = n;
+            break;
         end
     end
-    
+
     % Reproduce
     reproduce = rand;
     
@@ -66,12 +65,17 @@ for t = 1:T
     end
     
     % If reproduction else death
-    m = 0;
-    while (m ~= s)
-        m = randi(length(S(:, 1)));
+    m = randi(N_spec);
+    while (m == s)
+        m = randi(N_spec);
     end
     
     if (reproduce < q)
+        
+        % Metabolites consumed during reproduction
+        R(i) = R(i) - 1;
+        R(j) = R(j) - 1;
+        
         % Mutate if smaller
         if (rand >= p)
             m = s;
@@ -79,36 +83,41 @@ for t = 1:T
         
         % Update
         if ((i+j) < (n+1))
-            S(m, t) = S(m, t) + 1;
+            S_tmp(m) = S_tmp(m) + 1;
             R(i+j) = R(i+j) + 1;
         else
-            S(m, t) = S(m, t) + 1;
+            S_tmp(m) = S_tmp(m) + 1;
             R(1) = R(1) + 1;
             if ((i+j) - (n+1) > 0)
                 R(1) = R(1) + 1;
             end
         end
         
-        % Metabolites consumed during reproduction
-        R(i) = R(i) - 1;
-        R(j) = R(j) - 1;
-        
-        S(s, t) = S(s, t) + 1;
+        S_tmp(s) = S_tmp(s) + 1;
     end
     
     % Individual dies
-    S(s, t) = S(s, t) - 1;
+    S_tmp(s) = S_tmp(s) - 1;
     
-    % Update
-    S(:, t+1) = S(:, t);
+    t_old = t;
+    t = t + dt;
+    if floor(t) - floor(t_old) == 1
+        t_tmp = floor(t);
+        %R(t_tmp,:) = resource;
+        S(:, t_tmp) = S_tmp;
+        N(t_tmp) = sum(S(:, t_tmp));
+        if (N(t_tmp) == 0)
+            break;
+        end
+    end
 end
-N(t) = sum(S(:, t));
+%N(t) = sum(S(:, t));
 disp(['standard deviation of N: ' num2str(std(N))]);
 close(h);
 
 % Plot population
 figure()
-area(1:t, S(:,1:t)');
+area(1:t_tmp, S(:, 1:t_tmp)');
 legend('1', '2', '3', '4', '5', '6');
 xlabel('Time')
 ylabel('Population')
